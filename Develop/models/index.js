f262f640293168c90d153fa0d9be908b74a575ad
yamilhp5 <yamil.hernandezp@gmail.com>
2024-05-05 16:42:49 -0400

    Fixed Models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,30 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey:'category_id'
+});
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey:'categoru_id',
+  onDelete: 'CASCADE',
+
+});
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany (Tag, {
+  through: ProductTag,
+  as: 'productTag_products',
+  foreignKey: 'product_id'
+});
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany (Product, {
+  through: ProductTag,
+  as: 'productTag_products', 
+  foreignKey: 'tag_id'
+});
 
 module.exports = {
   Product,
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -1,5 +1,4 @@
 const router = require('express').Router();
-const e = require('express');
 const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
